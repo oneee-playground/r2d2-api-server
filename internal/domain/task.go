db7643d03f0f3619fbd6eba6f260613d29ff7294
onee-only <kimww0306@gmail.com>
2024-05-15 12:44:31 +0900

    Add task domain interfaces

--- internal/domain/task.go
+++ internal/domain/task.go
@@ -1,13 +1,21 @@
 package domain
 
-import "github.com/google/uuid"
+import (
+	"context"
+	"errors"
 
-type TaskStage uint8
+	"github.com/google/uuid"
+	"github.com/oneee-playground/r2d2-api-server/internal/domain/dto"
+)
+
+//go:generate mockgen -source=task.go -destination=../../test/mocks/task.go -package=mocks
+
+type TaskStage string
 
 const (
-	StageDraft TaskStage = iota
-	StageAvailable
-	StageFixing
+	StageDraft     TaskStage = "DRAFT"
+	StageAvailable TaskStage = "AVAILABLE"
+	StageFixing    TaskStage = "FIXING"
 )
 
 type Task struct {
@@ -15,6 +23,23 @@ type Task struct {
 	Title       string
 	Description string
 	Stage       TaskStage
+}
 
-	Sections []*Section
+type TaskUsecase interface {
+	GetList(ctx context.Context) (out *dto.TaskListOutput, err error)
+	GetTask(ctx context.Context, in dto.IDInput) (out *dto.TaskOutput, err error)
+	CreateTask(ctx context.Context, in dto.TaskInput) (out *dto.IDOutput, err error)
+	UpdateTask(ctx context.Context, in dto.UpdateTaskInput) (err error)
+	ChangeStage(ctx context.Context, in dto.TaskStageInput) (err error)
+}
+
+var (
+	ErrTaskNotFound = errors.New("task not found")
+)
+
+type TaskRepository interface {
+	FetchAll(ctx context.Context) ([]Task, error)
+	FetchByID(ctx context.Context, id uuid.UUID) (Task, error)
+	Create(ctx context.Context, task Task) error
+	Update(ctx context.Context, task Task) error
 }
