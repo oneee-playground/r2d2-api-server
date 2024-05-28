29a458e4e52719d9d7700becbbb10332e487b6e0
onee-only <kimww0306@gmail.com>
2024-05-28 17:52:03 +0900

    Change event interfaces

--- internal/global/event/event.go
+++ internal/global/event/event.go
@@ -11,15 +11,24 @@ type Topic string
 
 const (
 	TopicSubmission Topic = "submission"
+	TopicBuild      Topic = "build"
+	TopicTest       Topic = "test"
 )
 
 // Event schema for TopicSubmission
 type SubmissionEvent struct {
-	ID           uuid.UUID
-	SubmissionID uuid.UUID
-	UserID       uuid.UUID
+	ID           uuid.UUID        `json:"id"`
+	SubmissionID uuid.UUID        `json:"submissionID"`
+	UserID       uuid.UUID        `json:"userID"`
+	Kind         domain.EventKind `json:"kind"`
+	Extra        string           `json:"extra"`
+	Timestamp    time.Time        `json:"timestamp"`
+}
 
-	Kind      domain.EventKind
-	Extra     string
-	Timestamp time.Time
+// Event schema for TopicBuild, TopicTest
+type ExecEvent struct {
+	ID      uuid.UUID     `json:"id"`
+	Success bool          `json:"success"`
+	Took    time.Duration `json:"took"`
+	Extra   string        `json:"extra"`
 }
