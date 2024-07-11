13d261f0b87bdd910bc5f1999aa39db2fa5cc530
onee-only <kimww0306@gmail.com>
2024-07-11 16:04:31 +0900

    Add Serve to http router

--- internal/infra/http/routes.go
+++ internal/infra/http/routes.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/oneee-playground/r2d2-api-server/internal/domain"
 	"github.com/oneee-playground/r2d2-api-server/internal/infra/http/handler"
@@ -9,7 +11,7 @@ import (
 	"go.uber.org/zap"
 )
 
-type RouteBuilder struct {
+type Router struct {
 	Engine *gin.Engine
 
 	TokenDecoder auth_module.TokenDecoder
@@ -26,14 +28,14 @@ type RouteBuilder struct {
 	AuthHandler       *handler.AuthHandler
 }
 
-func (b *RouteBuilder) Build() {
-	router := b.Engine
+func (r *Router) Build() {
+	router := r.Engine
 
 	// Middlewares
 	var (
-		authFilter    = middleware.NewAuthFilter(b.TokenDecoder)
-		errorHandler  = middleware.NewErrorHandler(b.ErrorLogger)
-		requestLogger = middleware.NewRequestLogger(b.RequestLogger)
+		authFilter    = middleware.NewAuthFilter(r.TokenDecoder)
+		errorHandler  = middleware.NewErrorHandler(r.ErrorLogger)
+		requestLogger = middleware.NewRequestLogger(r.RequestLogger)
 	)
 
 	var (
@@ -50,56 +52,60 @@ func (b *RouteBuilder) Build() {
 
 	auth := router.Group("/auth")
 	{
-		auth.POST("/oauth/github", b.AuthHandler.HandleSignIn)
+		auth.POST("/oauth/github", r.AuthHandler.HandleSignIn)
 	}
 
 	user := router.Group("/users")
 	{
-		user.GET("/me", authRequired, memberOnly, b.UserHandler.HandleSelfInfo)
+		user.GET("/me", authRequired, memberOnly, r.UserHandler.HandleSelfInfo)
 	}
 
 	task := router.Group("/tasks")
 	{
-		task.GET("", b.TaskHandler.HandleGetList)
-		task.POST("", authRequired, adminOnly, b.TaskHandler.HandleCreateTask)
+		task.GET("", r.TaskHandler.HandleGetList)
+		task.POST("", authRequired, adminOnly, r.TaskHandler.HandleCreateTask)
 
 		oneTask := task.Group("/:id")
 		{
-			oneTask.GET("", b.TaskHandler.HandleGetTask)
-			oneTask.PUT("", authRequired, adminOnly, b.TaskHandler.HandleUpdateTask)
-			oneTask.PATCH("", authRequired, adminOnly, b.TaskHandler.HandleChangeStage)
+			oneTask.GET("", r.TaskHandler.HandleGetTask)
+			oneTask.PUT("", authRequired, adminOnly, r.TaskHandler.HandleUpdateTask)
+			oneTask.PATCH("", authRequired, adminOnly, r.TaskHandler.HandleChangeStage)
 		}
 	}
 
 	section := router.Group("/tasks/:id/sections")
 	{
-		section.GET("", b.SectionHandler.HandleGetList)
-		section.POST("", authRequired, adminOnly, b.SectionHandler.HandleCreateSection)
+		section.GET("", r.SectionHandler.HandleGetList)
+		section.POST("", authRequired, adminOnly, r.SectionHandler.HandleCreateSection)
 	}
 
 	oneSection := router.Group("/tasks/:taskID/sections/:sectionID")
 	{
-		oneSection.PUT("", authRequired, adminOnly, b.SectionHandler.HandleUpdateSeciton)
-		oneSection.PATCH("/index", authRequired, adminOnly, b.SectionHandler.HandleChangeIndex)
+		oneSection.PUT("", authRequired, adminOnly, r.SectionHandler.HandleUpdateSeciton)
+		oneSection.PATCH("/index", authRequired, adminOnly, r.SectionHandler.HandleChangeIndex)
 	}
 
 	resource := router.Group("/tasks/:id/resources")
 	{
-		resource.GET("", b.ResourceHandler.HandleGetList)
-		resource.POST("", authRequired, adminOnly, b.ResourceHandler.HandleCreateResource)
-		resource.DELETE("/:name", authRequired, adminOnly, b.ResourceHandler.HandleDeleteResource)
+		resource.GET("", r.ResourceHandler.HandleGetList)
+		resource.POST("", authRequired, adminOnly, r.ResourceHandler.HandleCreateResource)
+		resource.DELETE("/:name", authRequired, adminOnly, r.ResourceHandler.HandleDeleteResource)
 	}
 
 	submission := router.Group("/tasks/:id/submissions")
 	{
-		submission.GET("", b.SubmissionHandler.HandleGetList)
-		submission.POST("", authRequired, memberOnly, b.SubmissionHandler.HandleSubmit)
+		submission.GET("", r.SubmissionHandler.HandleGetList)
+		submission.POST("", authRequired, memberOnly, r.SubmissionHandler.HandleSubmit)
 	}
 
 	oneSubmission := router.Group("/tasks/:taskID/submissions/:submissionID")
 	{
-		oneSubmission.PATCH("", authRequired, adminOnly, b.SubmissionHandler.HandleDecideApproval)
-		oneSubmission.DELETE("", authRequired, memberOnly, b.SubmissionHandler.HandleCancel)
-		oneSubmission.GET("/events", b.EventHandler.HandleGetAll)
+		oneSubmission.PATCH("", authRequired, adminOnly, r.SubmissionHandler.HandleDecideApproval)
+		oneSubmission.DELETE("", authRequired, memberOnly, r.SubmissionHandler.HandleCancel)
+		oneSubmission.GET("/events", r.EventHandler.HandleGetAll)
 	}
+}
+
+func (r *Router) Serve(port int) error {
+	return r.Engine.Run(fmt.Sprintf(":%d", port))
 }
